5c8b0e5d69e18b9e923c7c721217c8669f36b58f
KireMitrov <kire_val@yahoo.com>
2022-09-04 23:19:30 +0200

    Improved cart-overlay-css

--- src/components/Cart Overlay/CartOverlay.js
+++ src/components/Cart Overlay/CartOverlay.js
@@ -9,26 +9,56 @@ function CartOverlay() {
     return (
         <div className="cart-overlay" >
             <div className="cart-overlay-products-container">
-                <div>My Bag, {cartItems.length} items</div>
-                <div>
-                    {cartItems.map((item) => (
+                <div><b>My Bag,</b> {cartItems.length} items</div>
+                {cartItems.map((item) => (
+                <div className="cart-overlay-item-container">
+                    <div className="cart-overlay-items-left">
                         <div>
-                            <div>{item.name}</div>
-                            <div>$50.00</div>
-                            <div>Size:</div>
-                            <div>Color:</div>
-                            <img className="cart-overlay-img" src={item.gallery[0]} alt={item.name}></img>
+                            <div className="cart-overlay-text">{item.brand}</div>
+                            <div className="cart-overlay-text">{item.name}</div>
+                            <div className="cart-overlay-product-price" >$50.00</div>
                         </div>
-                    ))}
+                        <div>
+                            {item.attributes.map((attribute, index) => {
+                                if (attribute.type === 'text')
+                                    return <div key={index}>
+                                        <p className="cart-overlay-attributes-text">{attribute.name}:</p>
+                                        <div className="attributes-container">
+                                            {attribute.items.map((value) => (
+                                                <div className="cart-overlay-attributes-rectangle" key={value.value}>{value.value}</div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                return <div key={index}>
+                                    <p className="cart-overlay-attributes-text">{attribute.name}:</p>
+                                    <div className="attributes-container">
+                                        {attribute.items.map((value) => (
+                                            <div className="cart-overlay-attributes-rectangle-color" style={{ backgroundColor: `${value.value}` }} key={value.value}></div>
+                                        ))}
+                                    </div>
+                                </div>
+                            }
+                            )}
+                        </div>
+                    </div>
+                        <div className="cart-overlay-items-right">
+                            <div className="cart-quantity-selector">
+                                <div className="cart-overlay-attributes-rectangle">+</div>
+                                <div className="cart-overlay-item-quantity">1</div>
+                                <div className="cart-overlay-attributes-rectangle">-</div>
+                            </div>
+                            <img className="cart-img" src={item.gallery[0]} alt={item.name}></img>
+                        </div>
                 </div>
+            ))}
                 <div className="cart-overlay-total">
                     <div className="cart-overlay-total-text">Total</div>
                     <div className="cart-overlay-price">$299,00</div>
                 </div>
-            </div>
             <div className="cart-overlay-btns">
                 <Link to="cart"><button className="cart-overlay-view-bag" onClick={() => (setCartIsOpen(false))} >VIEW BAG</button></Link>
                 <button className="cart-overlay-check-out">CHECK OUT</button>
+            </div>
             </div>
         </div>
     )
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -42,15 +42,23 @@ function Cart() {
                             )}
                         </div>
                     </div>
-                        <div>
+                        <div className="cart-items-right">
+                            <div className="cart-quantity-selector">
+                                <div className="cart-minus-plus-squares">+</div>
+                                <div className="cart-item-quantity">1</div>
+                                <div className="cart-minus-plus-squares">-</div>
+                            </div>
                             <img className="cart-img" src={item.gallery[0]} alt={item.name}></img>
                         </div>
                         <div className="cart-line-container"></div>
                 </div>
             ))}
-            <div>Quantity: {cartItems.length}</div>
-            <div>Total: $300.00</div>
-            <button className="cart-order-btn">ORDER</button>
+            <div className="cart-order-container">
+                <div>Tax 21%: $21.00</div>
+                <div>Quantity: {cartItems.length}</div>
+                <div>Total: $300.00</div>
+                <button className="cart-order-btn">ORDER</button>
+            </div>
         </div>
     )
 }
